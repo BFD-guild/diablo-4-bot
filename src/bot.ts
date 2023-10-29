72b6582fcd9548355c159e32f2cc7c4dddbaa44f
Bryant Davis <bryant.davis@hotmail.com>
2023-10-28 23:28:20 -0700

    feat: added re-usabled private mthods for add and remove

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { BaseChannel, ChannelType, Client, Message } from "discord.js";
+import { BaseChannel, ChannelType, Client, Message, Presence } from "discord.js";
 import { inject, injectable } from "inversify";
 import { TYPES } from "./types";
 
@@ -15,38 +15,51 @@ export class Bot {
     this.token = token;
   }
 
+  private remove(newPres: Presence){
+    newPres.guild.roles.fetch().then((x) => {
+      for (const item of x) {
+        const role = item[1];
+        if (role.name === "Active") {
+          newPres.member.roles.remove(role, "Playing Diablo 4");
+        }
+      }
+    });
+  }
+
+  private add(newPres: Presence){
+    newPres.guild.roles.fetch().then((x) => {
+      for (const item of x) {
+        const role = item[1];
+        if (role.name === "Active") {
+          newPres.member.roles.add(role, "Playing Diablo 4");
+          let guildChannels = newPres.guild.channels;
+          let channelName = 'activity-feed';
+          let channel = guildChannels.cache.find(channel => channel.name === channelName);
+          if (channel && channel.type === ChannelType.GuildText) {
+            const message = `${newPres.user.username} has started playing Diablo IV on ${new Date().toString()}`
+              channel.send(message);
+          }
+        }
+      }
+    });
+  }
+
   public listen(): Promise<string> {
     this.client.on("presenceUpdate", (_, newPres) => {
       if (newPres !== null && newPres.member !== null) {
         const activities = newPres.member.presence.activities;
         if (!activities.length) {
-          newPres.guild.roles.fetch().then((x) => {
-            for (const item of x) {
-              const role = item[1];
-              if (role.name === "Active") {
-                newPres.member.roles.remove(role, "Playing Diablo 4");
-              }
-            }
-          });
+          this.remove(newPres)
         }
+        let isPlayingDiablo = false;
         for (const activity of activities) {
           if (activity.name === "Diablo IV") {
-            newPres.guild.roles.fetch().then((x) => {
-              for (const item of x) {
-                const role = item[1];
-                if (role.name === "Active") {
-                  newPres.member.roles.add(role, "Playing Diablo 4");
-                  let guildChannels = newPres.guild.channels;
-                  let channelName = 'activity-feed';
-                  let channel = guildChannels.cache.find(channel => channel.name === channelName);
-                  if (channel && channel.type === ChannelType.GuildText) {
-                    const message = `${newPres.user.username} has started playing Diablo IV on ${new Date().toString()}`
-                      channel.send(message);
-                  }
-                }
-              }
-            });
+            this.add(newPres)
+            isPlayingDiablo = true;
           }
+        }
+        if(!isPlayingDiablo){
+          this.remove(newPres)
         }
       }
     });

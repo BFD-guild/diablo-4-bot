aad1a19a2ea317c7512aaa5e2f969893066aa1fa
Bryant Davis <bryant.davis@hotmail.com>
2023-10-28 19:12:19 -0700

    feat: added the initial workings of the active role swappig

---
+++ src/inversify.config.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import { Container } from "inversify";
+import { Client, GatewayIntentBits } from "discord.js";
+import { Bot } from "./bot";
+import { TYPES } from "./types";
+const container = new Container();
+container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
+container.bind<Client>(TYPES.Client).toConstantValue(
+  new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildPresences,
+    ],
+  })
+);
+container.bind<string>(TYPES.Token).toConstantValue(process.env.TOKEN);
+
+const stuff = "stuff";
+const arr = [];
+
+export default container;
